ae51306431f5ed93951a992236fc908c7558e846
jiangrenjie <jiangrenjie@hhu.edu.cn>
2022-06-30 13:04:16 +0800

    封装了Icon组件

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import {Link} from "react-router-dom";
 import React from "react";
+import Icon from "./Icon";
 
 require('icons/labels.svg');
 require('icons/money.svg');
@@ -21,26 +22,21 @@ const NavWrapper = styled.nav`
     }
   }
 `
+
 const Nav = () => {
     return (
         <NavWrapper>
             <ul>
                 <li>
-                    <svg className="icon">
-                        <use xlinkHref="#labels" />
-                    </svg>
+                    <Icon name="labels" />
                     <Link to="/tags">标签</Link>
                 </li>
                 <li>
-                    <svg className="icon">
-                        <use xlinkHref="#money" />
-                    </svg>
+                    <Icon name="money" />
                     <Link to="/money">记账</Link>
                 </li>
                 <li>
-                    <svg className="icon">
-                        <use xlinkHref="#statistics" />
-                    </svg>
+                    <Icon name="statistics" />
                     <Link to="/statistics">统计</Link>
                 </li>
             </ul>
