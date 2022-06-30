13de0c917d07668bfa0a6a6883e080efd86ea9a2
jiangrenjie <jiangrenjie@hhu.edu.cn>
2022-06-30 18:35:29 +0800

    完成tagsSection UI

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,33 @@ import { HashRouter, Routes, Route, } from "react-router-dom";
 import Tags from "./views/Tags";
 import Money from "./views/Money";
 import Statistics from "./views/Statistics";
+import styled from "styled-components";
+
+const AppWrapper = styled.div`
+  color: #333;
+`;
 
 function App() {
   return (
-      <HashRouter>
-                  <Routes>
-                      <Route path="/" element={<Money />} />
-                      <Route path="/tags" element={<Tags />} />
-                      <Route path="/money" element={<Money />} />
-                      <Route path="/statistics" element={<Statistics />} />
-                      <Route
-                          path="*"
-                          element={
-                              <main style={{ padding: "1rem" }}>
-                                  <p>There's nothing here!</p>
-                              </main>
-                          }
-                      />
-                  </Routes>
-      </HashRouter>
+      <AppWrapper>
+          <HashRouter>
+              <Routes>
+                  <Route path="/" element={<Money />} />
+                  <Route path="/tags" element={<Tags />} />
+                  <Route path="/money" element={<Money />} />
+                  <Route path="/statistics" element={<Statistics />} />
+                  <Route
+                      path="*"
+                      element={
+                          <main style={{ padding: "1rem" }}>
+                              <p>There's nothing here!</p>
+                          </main>
+                      }
+                  />
+              </Routes>
+          </HashRouter>
+      </AppWrapper>
+
   );
 }
 
