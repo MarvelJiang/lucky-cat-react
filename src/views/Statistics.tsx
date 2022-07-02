be8eda8d0d66fd67881ffd3ebabf2cc80e2d72a0
jiangrenjie <jiangrenjie@hhu.edu.cn>
2022-07-02 20:05:33 +0800

    展示记录样式

--- src/views/Statistics.tsx
+++ src/views/Statistics.tsx
@@ -3,7 +3,22 @@ import React, {useState} from "react";
 import TypesSection from "./Money/TypesSection";
 import useRecords from "../hooks/useRecords";
 import useTags from "../hooks/useTags";
-import day from "dayjs"
+import day from "dayjs";
+import styled from "styled-components";
+
+const Item = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background: white;
+  font-size: 18px;
+  line-height: 20px;
+  padding:10px 16px;
+  > .note{
+    margin-right: auto;
+    margin-left: 16px;
+    opacity: .7;
+  }
+`
 
 const Statistics = () => {
     const [type,setType] = useState('支出')
@@ -15,13 +30,18 @@ const Statistics = () => {
 
             <div>
                 {records.map(r=>
-                    <div>
-                        <span>{day(r.createdAt).format('YYYY年MM月DD日')}</span>
-                        <hr/>
-                        <span>{getName(r.tagId)}</span>
-                        <span>{r.note}</span>
-                        <span>￥{r.amount}</span>
-                    </div>
+                    <Item>
+                        {/*<span>{day(r.createdAt).format('YYYY年MM月DD日')}</span>*/}
+                        <div className="tags">
+                            <span>{getName(r.tagId)}</span>
+                        </div>
+                        <div className="note">
+                            <span>{r.note}</span>
+                        </div>
+                        <div className="amount">
+                            <span>￥{r.amount}</span>
+                        </div>
+                    </Item>
                 )}
             </div>
         </Layout>
