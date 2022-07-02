97c57978beda058c64cee20fd8f232b54218127e
jiangrenjie <jiangrenjie@hhu.edu.cn>
2022-07-02 11:58:30 +0800

    完成编辑标签功能

--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -38,9 +38,9 @@ const InputWrapper = styled.div`
 `
 
 const Tag:React.FC = () => {
-    const {findTag} = useTags();
-    let {id} = useParams<Params>();
-    const tag = findTag(parseInt(id!));
+    const {findTag,updateTag} = useTags();
+    let {id:idString} = useParams<Params>();
+    const tag = findTag(parseInt(idString!));
     return (
         <Layout>
             <Tabbar>
@@ -49,7 +49,10 @@ const Tag:React.FC = () => {
                 <Space />
             </Tabbar>
             <InputWrapper>
-                <Input label="标签名" type="text" placeholder="标签名..." value={tag.name}/>
+                <Input label="标签名" type="text" placeholder="标签名..." value={tag.name}
+                onChange={(e)=>{
+                    updateTag(tag.id,{name:e.target.value})
+                }}/>
             </InputWrapper>
             <Center>
                 <Space/>
